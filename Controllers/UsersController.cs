58143cd77ef71b5f8c25c302cac323b0fc7bfcfa
Ilya Radchenko <ilya@burstcreations.com>
2016-11-07 12:56:50 -0500

    Use two user routes

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TestApi.PrototypeApi;
+
+namespace TestApi.Controllers {
+  [Route("api/[controller]")]
+  public class UsersController : Controller {
+    private readonly ApiContext db;
+    public UsersController(ApiContext context) {
+      db = context;
+    }
+
+    [HttpGet]
+    public async Task<Object> GetAll() {
+      var users = await db.Users.ToArrayAsync();
+      return new { users };
+    }
+
+    // GET: api/users/ or /api/users/123
+    [HttpGet("{id:int}")]
+    public async Task<Object> GetById(int id = 0) {
+      var user = await db.Users.SingleAsync(u => u.id == id);
+      return new { user };
+    }
+  }
+}
